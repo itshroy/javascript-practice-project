b9e9c9f4957df0cb155ef31a2b7ddb7324375d8a
itshroy <naimish253@gmail.com>
2026-07-04 12:39:36 +0530

    updated Todo with DOM

--- TodoApp/todoapp.js
+++ TodoApp/todoapp.js
@@ -1,30 +1,10 @@
-let todo = [];
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("input");
 
-let req = prompt("Enter your request :");
-
-while(true){
-    if (req == "quit") {
-        console.log("You quit the app.");
-        break;
-    }
-
-    if(req == "list"){
-        console.log("----------------");
-        for(let i=0; i<todo.length; i++){
-            console.log(i, todo[i]);
-        }
-        console.log("----------------");
-    } else if (req == "add"){
-        let task = prompt("Enter the task you want to add :");
-        todo.push(task);
-        console.log("task added");
-    } else if(req == "delete"){
-        let idx = prompt("Please enter the index you want to delete :");
-        todo.splice(idx, 1);
-        console.log("Task deleted!");
-    } else {
-        console.log("Wrong request.");
-    }
-
-    req = prompt("Enter your request :");
-}
+btn.addEventListener("click", function(){
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+    ul.appendChild(item);
+    inp.value = "";
+})
